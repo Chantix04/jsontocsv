eaaeb2be58840ff687e2fce3796a5ba1bc8e1603
Chantix04 <gstnrcld@gmail.com>
2023-11-07 12:23:57 -0300

    segundo try al csv

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pandas as pd
-obj = pd.read_json('message.json',orient='records')
+obj = pd.read_json('message.json',orient='columns')
 print(obj)
 obj.index+=1
 obj.to_csv('message_values.csv')
